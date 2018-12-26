0c52e685eeb1b63c360afdd46930e518cb9ef9dc
Matt Galvin <mgalvin@endpoint.com>
2018-12-25 22:15:08 -0500

    Update package-lock and db config

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "matt",
+  password: "",
   database: "hello_dev",
   hostname: "localhost",
   pool_size: 10
